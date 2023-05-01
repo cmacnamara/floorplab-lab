eac675e0bbb9989f9f139a2bf4657f9a6777cfac
Chris MacNamara <cpmacnamara@gmail.com>
2023-05-01 14:49:49 -0400

    separate kitchen subcomponents into kitchen component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import FloorPlan from './FloorPlan'
+import Oven from './Oven'
+import Sink from './Sink'
 
 function App() {
   return (
---
+++ src/Kitchen.jsx
@@ -0,0 +1,13 @@
+import Oven from './Oven'
+import Sink from './Sink'
+
+const Kitchen = props => {
+  return (
+    <div>
+      <Oven />
+      <Sink />
+    </div>
+  )
+}
+
+export default Kitchen
